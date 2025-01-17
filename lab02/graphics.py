ea6cbf8412ce58c0a9b896fb9439e9bb89904183
gprielipp2026 <m265112@usna.edu>
2025-01-17 13:45:21 -0500

    commented graphics

--- lab02/graphics.py
+++ lab02/graphics.py
@@ -5,7 +5,7 @@ import numpy
 
 import math
 
-# The beginnings of a Vector3D - For you to edit
+# The beginnings of a Vector3D
 class Vector3D:
     def __init__(self, val, *args):
         if type(val) is numpy.ndarray:
@@ -17,23 +17,30 @@ class Vector3D:
     
     def __str__(self):
         return str(self.v)
-    
+   
+    # add two vectors together
     def __add__(self, other):
         return Vector3D(self.v + other.v)
-
+    
+    # subtract two vectors
     def __sub__(self, other):
         return Vector3D(self.v - other.v)
-
+    
+    # take the dot product of two vectors
     def dot(self, other):
         return self.v.dot(other.v) 
-
+    
+    # return the dot product from an angle and vector 
     def dotangle(self, other, angle):
         # angle is in radians
         return self.magnitude() * other.magnitude() * math.cos(angle) 
-
+    
+    # scale a vector
     def scalar(self, a):
         return Vector3D(self.v * a) 
-
+    
+    # Allows the notation of v * a and v * u (vector * scalar or vector *
+    # vector)
     def __mul__(self, other):
         if type(other) in [int, float]:
             return self.scalar(other)
@@ -41,23 +48,28 @@ class Vector3D:
             return self.dot(other)
         else:
             raise Exception(f'Error: {other} does not work for multiplication')
-
+    
+    # divide a vector by a scalar
     def __div__(self, a):
         return Vector3D(self.v / a) 
-
+    
+    # return a new vector that has the same values
     def copy(self):
         return Vector3D(self.v.copy()) 
-
+    
+    # take the magnitude
     def magnitude(self):
         return numpy.sqrt(self.square()) 
 
+    # x^2 + y^2 + z^2
     def square(self):
         return (self.v * self.v).sum() 
 
+    # return the cross product
     def cross(self, other):
         return Vector3D(numpy.cross(self.v, other.v)) 
 
-
+# represents a point in 3D space
 class Point3D:
     def __init__(self, v, *args):
         if isinstance(v, numpy.ndarray):
@@ -69,29 +81,41 @@ class Point3D:
     def __str__(self):
         return str(self.v)
     
+    # add a vector and point
     def __add__(self, other):
-        return Point3D(self.v + other.v)
-
+        if isinstance(other, Vector3D):
+            return Point3D(self.v + other.v)
+        else:
+            raise Exception(f'cannot add a {type(other)} to a Point3D')
+    
+    # allows notation of p1 - p2 = vector or point - vector = point
     def __sub__(self, other):
         if isinstance(other, Vector3D):
             return Point3D(self.v - other.v)
         elif isinstance(other, Point3D):
             return Vector3D(self.v - other.v)
-
+    
+    # the distance squared to a given point
     def distancesquared(self, point):
         vector = self - point
         return vector.square()
-
+    
+    # the distance between this point and a given point
     def distance(self, point):
         vector = self - point
         return vector.magnitude()
     
+    # get a Point with the same x,y,z
     def copy(self):
         return Point3D(self.v.copy())
 
     def __mul__(self, constant):
-        return Point3D(self.v * constant)
+        if type(constant) in [int, float]:
+            return Point3D(self.v * constant)
+        else:
+            raise Exception(f'cannot multiply a Point3D by a {type(constant)}')
 
+# A Normal for defining surfaces
 class Normal:
     def __init__(self, v, *args):
         if isinstance(v, numpy.ndarray):
@@ -104,22 +128,35 @@ class Normal:
             raise Exception('invalid arguments passed to Normal')
     def __str__(self):
         return str(self.v)
-        
+    
+    # returns Normal(-x,-y,-z)
     def __neg__(self):
         return Normal(-self.v)
-
+    
+    # performs vector addition
     def __add__(self, other):
         if isinstance(other, Vector3D):
             return Vector3D(self.v + other.v)
         elif isinstance(other, Normal):
             return Normal(self.v + other.v)
-   
+
+    # takes the dot product of two normals or a normal and a vector
     def dot(self, other):
-        return self.v.dot(other.v)
+        if type(other) in [Vector3D, Normal]:
+            return self.v.dot(other.v)
+        else:
+            raise Exception(f'Normal.dot({type(other)}) is not defined')
 
+    # scalar multiply a Normal
     def scalar(self, other):
-        return Normal(self.v * other)
-
+        if type(other) in [int, float]:
+            return Normal(self.v * other)
+        else:
+            raise Exception(f'Cannot scalar multiply a Normal with a
+            {type(other)}')
+    
+    # convenient notation to allow the scalar and dot product to piggyback on
+    # the '*' operator
     def __mul__(self, other):
         if type(other) in [Vector3D, Normal]:
             return self.dot(other)
